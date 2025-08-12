eed70d807a7f0a74bd4796c771df0ff3d0954e7b
GianlucaTurra <gianlucaturra1@gmail.com>
2025-08-12 17:15:29 +0200

    Sql scripts: working_directory not null

--- sql_scripts/sessions.sql
+++ sql_scripts/sessions.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS Sessions (
     id INTEGER PRIMARY KEY,
     name NOT NULL UNIQUE,
-    working_directory TEXT
+    working_directory TEXT NOT NULL
 )
--- sql_scripts/windows.sql
+++ sql_scripts/windows.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS Windows (
     id INTEGER PRIMARY KEY,
     name TEXT NOT NULL,
-    working_directory TEXT
+    working_directory TEXT NOT NULL
 )
