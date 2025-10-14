77651a72e27ae13da9fcb3fb6bcf5d7d0a0731a0
GianlucaTurra <gianlucaturra1@gmail.com>
2025-10-15 00:09:16 +0200

    Chore: removed useless files

--- jmrp.teamux.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-tmux new -s "JMRP" -d -n "Back-End" -c /home/giatur/git/jmrp/
-tmux neww -d -n "Front-End" -c /home/giatur/git/jmrp-ui/ -t "JMRP"
-tmux neww -d -n "Other" -c "$HOME" -t "JMRP"
-tmux split-window -t "JMRP:Back-End" -h -l 25% -c /home/giatur/.config/jmrp
-tmux split-window -t "JMRP:Front-End" -v -l 23% -c /home/giatur/git/jmrp-ui/
