21af099bd45b4015d625f8aeec40a362fb7ecfcd
GianlucaTurra <gianlucaturra1@gmail.com>
2025-11-04 17:04:27 +0100

    Windows: refactor with utilities

--- tmux/windows.go
+++ tmux/windows.go
@@ -9,7 +9,7 @@ import (
 // NewWindow creates a new window in the current session
 func NewWindow(name string, workingDirectory string) error {
 	baseCmd := fmt.Sprintf("tmux new-window -d -n \"%s\"", name)
-	return newWindow(workingDirectory, baseCmd)
+	return commandWithWorkDir(workingDirectory, baseCmd)
 }
 
 // NewWindowWithTarget creates a new window in the given target session
@@ -18,22 +18,7 @@ func NewWindowWithTarget(name string, workingDirectory string, target string) er
 		return errors.New("missing window tartget")
 	}
 	baseCmd := fmt.Sprintf("tmux neww -t %s -d -n \"%s\"", target, name)
-	return newWindow(workingDirectory, baseCmd)
-}
-
-func newWindow(workingDirectory string, cmd string) error {
-	var nsdErr error
-	if err := checkDirectory(workingDirectory); err != nil {
-		nsdErr = NoSuchDirectoryError{"working directory doesn't exist"}
-	}
-	if strings.TrimSpace(workingDirectory) != "" {
-		cmd += fmt.Sprintf(" -c %s", workingDirectory)
-	}
-	if err := executeCommand(cmd); err != nil {
-		return err
-	} else {
-		return nsdErr
-	}
+	return commandWithWorkDir(workingDirectory, baseCmd)
 }
 
 // KillWindow FIXME: if the windows is in a detached session the name should be `Session:Window`
